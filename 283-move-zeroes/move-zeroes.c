b2ae5d10c9ac42864aae0ad49b031c2c74097f08
Vaarid Gupta <vaaridgupta07@gmail.com>
2026-09-17 18:55:38 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (9.35%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.c
@@ -0,0 +1,20 @@
+void moveZeroes(int* nums, int numsSize) {
+int j=-1;
+int temp;
+for(int i=0;i<numsSize;i++){
+    if(nums[i]==0){
+        j=i;
+        break;
+    }
+}
+
+if(j==-1) return ;
+for(int i=j+1;i<numsSize;i++){
+    if(nums[i]!=0){
+        temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+        j++;
+    }
+}
+}
